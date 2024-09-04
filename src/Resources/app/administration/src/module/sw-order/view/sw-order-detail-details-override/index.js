5995be78c1d2f70a86fe0bacbf6cf6fa9f47b5a9
artursalnik <artur.shopware.dev@gmail.com>
2024-09-04 17:28:06 +0300

    Add the condition for payment/shipping

--- src/Resources/app/administration/src/module/sw-order/view/sw-order-detail-details-override/index.js
+++ src/Resources/app/administration/src/module/sw-order/view/sw-order-detail-details-override/index.js
@@ -1,5 +1,18 @@
 import template from './sw-order-detail-details.html.twig';
 
 Shopware.Component.override('sw-order-detail-details', {
-    template
+    template,
+
+    computed: {
+        disableChangePayment() {
+            if (this.transaction.stateMachineState.technicalName !== 'open') {
+                return true
+            }
+        },
+        disableChangeShipping() {
+            if (this.order.deliveries.first().stateMachineState.technicalName !== 'open') {
+                return true
+            }
+        }
+    }
 });
